9d9a9398db917aa1b782f178d315e3706caad12a
Aggelos Papadakis <theakero@gmail.com>
2022-03-15 21:02:52 +0200

    Lab mexri pou mas pige se conio.h kai ta windows eginan aparaithta bleh

---
+++ lab4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    // cout << 'A' + 1 << endl;
+    int a,b;
+    char ch;
+    ch = 'A';
+    b =  ch;
+    cout << b;
+    a = ch + 2;
+    putch('a');
+    // getch()
+    // putch()
+    //conio.h
+   
+}
